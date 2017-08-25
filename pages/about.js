b5eca4767acf37925d33b1298a990f676dc48983
Benjamin Boruff <ben@boruff.me>
2017-08-25 16:07:03 -0400

    Edited about page

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout';
 export default () => (
   <div>
     <Layout>
-      <p>NA NA NA NA NA NA NA NA NA NA NA NA NA NA BATMAN!</p>
+      <p>NA NA NA NA NA NA NA NA NA NA NA NA NA NA BATMAN!!</p>
     </Layout>
   </div>
 );
